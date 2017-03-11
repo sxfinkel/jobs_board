41a1c9ad65f3cdd09c571d735690adba375e6fea
Susan Barrows <sxbarrows@gmail.com>
2017-03-11 21:43:36 +0000

    added job board files

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,45 @@
+class JobsController < ApplicationController
+   def index
+    @jobs = Job.all
+   end
+
+   def new
+      @job = Job.new
+   end
+
+   def edit
+    @job = Job.find(params[:id])
+   end
+
+   def create
+      Job.create(job_params)
+      redirect_to jobs_path
+   end
+
+   def show
+    @job = Job.find(params[:id])
+    render json: {
+      title: @job.title,
+      description: @job.description
+    }
+   end
+
+   def update
+    @job = Job.find(params[:id])
+    @job.update_attributes(job_params)
+    redirect_to jobs_path
+   end
+
+   def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+   end
+
+   private
+
+   def job_params
+    params.require(:job).permit(:title, :description)
+   end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+    root 'jobs#index'
+    resources :jobs
+end
